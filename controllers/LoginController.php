39ee6d703b951d6a385a84ca607b57713f776024
angelByte <angelstudio424@gmail.com>
2026-01-06 10:26:20 -0600

    Se agrego una nueva carpeta auth la cual contiene el login y crear, se modifico el arhcivo loginController.php para generar el render de las vistas

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,26 +2,38 @@
 
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController
 {
-    public static function login()
+    public static function login(Router $router)
     {
-        echo "desde el login";
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
+
+        //Render a la vista
+        $router->render('auth/login', [
+            'titulo' => 'Inicar Sesión',
+        ]);
     }
     public static function logout()
     {
         echo "SAliendo ....";
     }
 
-    public static function crear()
+    public static function crear(Router $router)
     {
-        echo "desde el crear";
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
+
+        //Render a la vista
+        $router->render('auth/crear', [
+            'titulo' => 'Crear Cuenta',
+        ]);
+
+
     }
     public static function olvide()
     {
